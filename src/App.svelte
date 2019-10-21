ec16067c749abb0e39a159582c672e81719444ce
Akifcan <48747986+Akifcan@users.noreply.github.com>
2019-10-21 16:23:47 +0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script type="text/javascript">
+
+	let name = null
+	let author = null
+
+	let songs = [
+		{
+			name: 'Good Vibe',
+			author: 'Jfla'
+		},
+		{
+			name: 'Different World',
+			author: 'Alan Walker'
+		},
+		{
+			name: 'City of stars',
+			author: 'La la land'
+		},
+		{
+			name: 'High hopes',
+			author: 'Panic! At The Disco'
+		}
+	]
+
+
+</script>
+
