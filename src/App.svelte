02547571c1051c2bc53aa2828d2c0bb48b902f7d
Akifcan <48747986+Akifcan@users.noreply.github.com>
2019-10-21 16:41:05 +0300

    mesajlar eklendi

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
 
 	let name = ''
 	let author = ''
+	let status = null
+
+	$: songsLength = songs.length
 
 	let songs = [
 		{
@@ -22,20 +25,34 @@
 		}
 	]
 
+	function showMessage(type, message){
+		return `<div class='alert alert-${type}'>${message}</div>`
+	}
+
 	function clearInputs(){
 		name = ''
 		author = ''
 	}
 
 	function addToSongList(){
-		songs = [...songs, {name: name, author: author}]
-		clearInputs()
+		if(songsLength<5){
+			songs = [...songs, {name: name, author: author}]
+			clearInputs()
+			status = 'success'
+		}else{
+			status = 'error'
+			clearInputs()
+		}
+		setTimeout(() => {
+			status = null
+		}, 2000)
 	}
 
 
 </script>
 
 <div class="container">
+	{songsLength}
 	<ul class="list-group">
 		{#each songs as song}
 			<li class="list-group-item">{song.name} <b class="float-right">{song.author}</b> </li>
@@ -47,5 +64,13 @@
 		<input type="text" class="form-control" bind:value={author} placeholder="Author">
 		<button class="btn btn-outline-primary btn-block" disabled={name=='' | author == ''}>Add</button>
 	</form>
+
+	{#if status}
+		{#if status == 'success'}
+			{@html showMessage('success', 'song added successfully')}
+			{:else}
+			{@html showMessage('danger', 'you can only add 5 song')}
+		{/if}	
+	{/if}
 
 </div>
